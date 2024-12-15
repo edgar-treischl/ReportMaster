caf7d94d99a424bde3feb66a28b6e0840034bf7b
Edgar Treischl <77931249+edgar-treischl@users.noreply.github.com>
2024-12-15 21:03:35 +0100

    add static website

---
+++ www/javascript.js
@@ -0,0 +1,19 @@
+document.addEventListener('DOMContentLoaded', function() {
+  const imageSelect = document.getElementById('image_select');
+  const selectedImage = document.getElementById('selected-image');
+
+  // Add event listener for dropdown change
+  imageSelect.addEventListener('change', function() {
+    // Get the selected value from dropdown (it should be the plot name without the extension)
+    const selectedImagePath = this.value;
+
+    // Dynamically build the image source path using JavaScript variables passed from R
+    const imagePath = basePath + snr + "_" + year + "/" + plotsDir + "/" + selectedImagePath + ".png";
+
+    // Update the image source dynamically based on the selected plot name
+    selectedImage.src = imagePath;
+
+    // Optional: Log the selected path to console for debugging
+    console.log('Selected Image Path:', selectedImage.src);
+  });
+});
